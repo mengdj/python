ec91abcbeeb1aa9a1330e36e31d31edb1cfed025
mengdejun <mengdj@outlook.com>
2018-09-14 15:04:03 +0800

    TCP segment of a reassembled PDU

--- pcap/pcap_test.py
+++ pcap/pcap_test.py
@@ -3,8 +3,6 @@
 __author__ = "mengdj@outlook.com"
 import unittest
 
-from proc.tcp_build import TCP_Build
-
 import pcap
 
 
@@ -15,25 +13,23 @@ def callback_tcp():
 class PacketTest(unittest.TestCase):
     def test_parse(self):
         p = pcap.Pcap()
-        it = p.parse("data/1.pcap")
-        tb=TCP_Build()
+        it = p.parse("data/3.pcap")
         for i in it:
+            print("")
             mac = i.data
             ip = mac.data
             if ip is not None:
                 ti = ip.data
                 if ti is not None:
-                    """
-                    print(mac)
+                    # print(mac)
                     print(i.head)
                     print(ip)
-                    """
                     if ti.__class__.__name__ == "TCP":
-                        tb.write(ti)
+                        print(ti.option)
                     elif ti.__class__.__name__ == "UDP":
                         pass
-                    #print(ti)
-        tb.show()
+                    print(ti)
+
 
 if __name__ == "__main__":
     unittest.main()
